1c4985ab512e09e183a22d918930ea9951e7f59e
sunny775 <sunny775@users.noreply.github.com>
2022-04-11 12:13:54 +0000

    Deploying to gh-pages from  @ 80e264001b7221790a60685a1b796c36b6bf8cde 🚀

---
+++ precache-manifest.34d5cee68f2fd5c16432675e27e9ff2d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b784f255bd3b0928df650de0064dc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03d8c924de3f40a70f20",
+    "url": "/static/css/3.d29f9f24.chunk.css"
+  },
+  {
+    "revision": "b4e982250473c1d3e1e6",
+    "url": "/static/css/4.7369bb08.chunk.css"
+  },
+  {
+    "revision": "3fcee26b71ad30fc1c90",
+    "url": "/static/css/main.836d0dce.chunk.css"
+  },
+  {
+    "revision": "6e127549d964022f0502",
+    "url": "/static/js/2.c82b43ee.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.c82b43ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d8c924de3f40a70f20",
+    "url": "/static/js/3.d5005196.chunk.js"
+  },
+  {
+    "revision": "b4e982250473c1d3e1e6",
+    "url": "/static/js/4.91c3ea4a.chunk.js"
+  },
+  {
+    "revision": "3fcee26b71ad30fc1c90",
+    "url": "/static/js/main.f5415575.chunk.js"
+  },
+  {
+    "revision": "e4f17c7755056fd69a33",
+    "url": "/static/js/runtime-main.05cfff9c.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "247b12756c0254acaca90151e8c1586d",
+    "url": "/static/media/blsLogo.247b1275.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "0234a6b27d56d9bc9c2398d90e115f70",
+    "url": "/static/media/debianLogo.0234a6b2.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "3c751a7bbfe6f73e362090eee6c27c3b",
+    "url": "/static/media/e-scholarsLogo.3c751a7b.png"
+  },
+  {
+    "revision": "4bb010ca99b17c2cecd6e30f36b3a533",
+    "url": "/static/media/freelanceLogo.4bb010ca.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
